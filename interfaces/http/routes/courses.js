53486457482e81cf6aa7e5352595c46d5af2042d
jcantillog <jcantillog@unicartagena.edu.co>
2018-10-16 20:58:55 -0500

    Setting routers.

---
+++ interfaces/http/routes/courses.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+
+var { coursesServices } = require('../../../services');
+
+/* GET courses listing. */
+router.get('/', function(req, res, next) {
+    try {
+        coursesServices.searchAllCourses(req, res, next);
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
+});
+
+router.get('/:page', function(req, res, next) {
+    try {
+        coursesServices.searchCoursesByName(req, res, next);
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
+});
+
+
+module.exports = router;
