de574f04da4c3f5881c2c3f22beebff3a4e3de9c
jcantillog <jcantillog@unicartagena.edu.co>
2018-10-16 20:44:04 -0500

    Initial project structure.

---
+++ app.js
@@ -0,0 +1,55 @@
+// importing global variables
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var mongoose = require('mongoose');
+var cors = require('cors');
+
+// setting routers
+var coursesRouter = require('./interfaces/http/routes/courses');
+
+// setting connection
+mongoose.connect
+(
+    'mongodb://root:bdatos1234@ds053459.mlab.com:53459/condorlabs',
+    {useNewUrlParser: true}
+);
+
+// setting express app
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// setting CORS
+app.use(cors());
+
+// setting endpoints
+app.use('/courses', coursesRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
