2ccf390379de5e52f5d2aca4ae9b2011f2b4f771
jcantillog <jcantillog@unicartagena.edu.co>
2018-10-17 13:38:13 -0500

    PUG views removed from app.js.

--- app.js
+++ app.js
@@ -20,10 +20,6 @@ mongoose.connect
 // setting express app
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
